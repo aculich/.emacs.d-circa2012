d1fec0333d60c2f2b9962381abae146a4c55bcb0
Aaron Culich <aculich@eecs.berkeley.edu>
2012-07-08 09:57:02 -0700

    make cursor and paren colors more visible by changing the color to red

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight ((t (:background "grey5"))))
+ '(show-paren-match ((t (:foreground "red"))))
+ '(show-paren-match-face ((t (:foreground "red"))) t))

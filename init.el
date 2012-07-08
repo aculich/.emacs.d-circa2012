f837f362862b3bd00938172d54cfb0495e9cdc7d
Aaron Culich <aculich@eecs.berkeley.edu>
2012-07-08 03:04:49 -0700

    enabled some more convenient keys

--- init.el
+++ init.el
@@ -3,5 +3,11 @@
 
 (put 'narrow-to-region 'disabled nil)
 
+(global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "C-q")     'kill-region)
+(global-set-key (kbd "C-w")     'backward-kill-word)
+(global-set-key (kbd "C-x b")   'list-buffers)
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
+
 (eval-after-load "package"
   '(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t))

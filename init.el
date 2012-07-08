a22603042f6c260077246996746528d8a0fd26bf
Aaron Culich <aculich@eecs.berkeley.edu>
2012-07-08 03:05:56 -0700

    enabled magit-status globally on C-c C-v

--- init.el
+++ init.el
@@ -11,3 +11,6 @@
 
 (eval-after-load "package"
   '(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t))
+
+(eval-after-load "magit"
+  '(global-set-key (kbd "C-c C-v") 'magit-status))

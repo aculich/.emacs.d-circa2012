ed2f4ba1006a8d77e6dda23078a1a9b1bf051387
Aaron Culich <aculich@eecs.berkeley.edu>
2012-07-08 03:03:48 -0700

    enabled narrow-to-region

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+(put 'narrow-to-region 'disabled nil)
+
 (eval-after-load "package"
   '(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t))
